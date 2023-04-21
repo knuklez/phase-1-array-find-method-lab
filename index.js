2b46b8d76dda9bcfa1e1cc1f86ad0e6f2cf99d9c
Anthony Lotson <focus_and_finish@yahoo.com>
2023-04-21 13:06:02 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(record){
+   for(let item of record){
+     if (item.result === "W"){ 
+  return item.year};
+}
+}
+
+//console.log(results.find(superbowlWin).year);
+
+/*it('returns a year the Denver Broncos won the superbowl', function () {
+    expect(superbowlWin(record)).to.equal('2015');
+      });
+    
+      it('returns undefined if the record has no win objects', function() {
+        const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+        expect(superbowlWin(sadReality)).to.equal(undefined)
+        */
